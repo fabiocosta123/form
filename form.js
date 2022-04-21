c540726aa6e4f29e5e269ece27ddf289f279f773
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2022-04-21 01:46:39 -0300

    capturando dados

---
+++ form.js
@@ -0,0 +1,27 @@
+// pega dados dos inputs e select
+let nome = document.getElementById('name').value
+let lastName = document.getElementById('lastName').value
+let email = document.getElementById('email').value
+console.log(nome, lastName, email);
+
+
+function recebeDados() {
+    let nome = document.getElementById('name').value
+    let lastName = document.getElementById('lastName').value
+    let email = document.getElementById('email').value
+    console.log(nome, lastName, email);
+    
+    /*const recDados = [{
+        nome: nome, sobrenome: lastName,
+        email: email
+    }]
+    return recDados*/
+    
+}
+
+
+
+
+
+
+
